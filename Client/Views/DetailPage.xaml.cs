c67092f01563def1d243c8859a67fc00b0b53878
Washington Xavier Yépez Yépez <wyepez@outlook.com>
2018-02-08 15:28:07 -0500

    Adding initial project

---
+++ Client/Views/DetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Client.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace Client.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
